7b95c4cc7fcad1722617434d25903bf382356929
Peter Levinsky <pele@zealand.dk>
2022-11-11 11:01:14 +0100

    Add project files.

---
+++ MFRA/Pages/Index.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MFRA.model;
+
+namespace MFRA.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public List<Person> Personer { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Personer = new List<Person>();
+            Personer.Add(new Person("Peter", "22334455"));
+            Personer.Add(new Person("Jakob", "22459033"));
+            Personer.Add(new Person("Vibeke", "99112455"));
+
+        }
+    }
+}
---
+++ MFRA/model/Person.cs
@@ -0,0 +1,23 @@
+﻿namespace MFRA.model
+{
+    public class Person
+    {
+        public Person():this("dummy","")
+        {
+        }
+
+        public Person(string navn, string tlf)
+        {
+            Navn = navn;
+            Tlf = tlf;
+        }
+
+        public String Navn { get; set; }
+        public String Tlf { get; set; }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Navn)}={Navn}, {nameof(Tlf)}={Tlf}}}";
+        }
+    }
+}
